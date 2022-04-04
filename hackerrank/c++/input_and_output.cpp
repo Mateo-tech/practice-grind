c2f601b871d681883f9e7a5542eb82836429a48b
mateo <mat.hladky@gmail.com>
2022-04-04 21:52:42 +0100

    Added leetcode sum of left leafs and created a hackerrank repo

---
+++ hackerrank/c++/input_and_output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int int1;
+	int int2;
+	int int3;
+	cin >> int1 >> int2 >> int3;
+	cout << int1 + int2 + int3;
+
+	return 0;
+}
