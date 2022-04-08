444afd526b97502ae25246280251971458f1b783
mateo <mat.hladky@gmail.com>
2022-04-08 09:25:32 +0100

    Added hackerrank/arrays_introduction.cpp

---
+++ hackerrank/c++/arrays_introduction.cpp
@@ -0,0 +1,19 @@
+#include <csmath>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int ;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = n - 1; i >= 0; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
